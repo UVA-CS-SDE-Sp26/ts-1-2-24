a6030ca339da3bd465be0744569c2c04e2ed2abc
WontonCoder <winstonfootball21@gmail.com>
2026-02-10 14:39:49 -0500

    Added output for 0 args

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,5 +1,30 @@
 /**
  * Commmand Line Utility
  */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class TopSecret {
+    public static void main(String[] args){
+
+    }
+
+}
+
+public static void outputFiles(Path filePath){
+    try (Stream<Path> stream = Files.list(filePath)) {
+        int fileNum = 1;
+        for (int i = 0; i < stream.length; i++){
+            String output = String.format("0%d "
+        }
+    } catch (IOException e) {
+        System.err.println("An I/O error occurred: " + e.getMessage());
+    }
+}
+
+public static void printContents(){
+
 }
